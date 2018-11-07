396727a56b3f9787632554f0c3a87028acc92fe7
TienYou-Nathan <nathan.tienyou@viacesi.fr>
2018-11-07 18:26:17 +0100

    Init bot

---
+++ main.js
@@ -0,0 +1,30 @@
+const Commando = require('discord.js-commando');
+const private = require('/token.json');
+
+const client = new Commando.Client({
+    owner: '268494575780233216'
+});
+
+const path = require('path');
+
+client.registry
+    // Registers your custom command groups
+    .registerGroups([
+        ['fun', 'Fun commands'],
+        ['some', 'Some group'],
+        ['other', 'Some other group']
+    ])
+
+    // Registers all built-in groups, commands, and argument types
+    .registerDefaults()
+
+    // Registers all of your commands in the ./commands/ directory
+    .registerCommandsIn(path.join(__dirname, 'commands'));
+
+const sqlite = require('sqlite');
+
+client.setProvider(
+    sqlite.open(path.join(__dirname, 'settings.sqlite3')).then(db => new Commando.SQLiteProvider(db))
+).catch(console.error);   
+
+client.login(private.token);
